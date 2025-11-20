1ad6af112a56d73638570d97af46c0e18407ad01
Gabriel Evangelista <evangelistagabriel798@gmail.com>
2025-11-20 10:22:28 -0300

    refactor: resolvendo identação em login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -38,11 +38,7 @@ export default function Login() {
         />
 
         <div className="relative flex flex-col justify-center items-center px-4 sm:px-8 md:px-12 py-10 bg-white/85 backdrop-blur-sm rounded-2xl lg:bg-transparent overflow-hidden z-10">
-
-          <h2 className="text-xl sm:text-2xl md:text-2xl font-semibold mb-8 text-center z-10">
-            Login
-          </h2>
-
+          <h2 className="text-xl sm:text-2xl md:text-2xl font-semibold mb-8 text-center z-10">Login</h2>
           <form
             onSubmit={handleLogin}
             className="w-full space-y-5 max-w-[300px] sm:max-w-[380px] md:max-w-[420px] z-10"
@@ -98,23 +94,15 @@ export default function Login() {
         </div>
 
         {/* LADO DIREITO */}
-        <div
-          className="relative hidden lg:flex flex-col justify-center items-center text-white px-8 pt-8 pb-10 overflow-hidden 
-                      bg-[url('src/assets/fundo.png')] bg-cover bg-center"
-        >
+        <div className="relative hidden lg:flex flex-col justify-center items-center text-white px-8 pt-8 pb-10 overflow-hidden bg-[url('src/assets/fundo.png')] bg-cover bg-center">
           <h3 className=" relative text-[25px] font-bold mb-8 text-center top-0.1">O futuro começa com a leitura!</h3>
-
           <div className="flex items-end gap-6 w-full justify-center mt-8">
-            <p className="text-[18px] leading-relaxed max-w-[200px] text-left mb-12">
-              "A sua biblioteca, a qualquer hora e em qualquer lugar. Faça seu login."
-            </p>
+            <p className="text-[18px] leading-relaxed max-w-[200px] text-left mb-12">"A sua biblioteca, a qualquer hora e em qualquer lugar. Faça seu login."</p>
             <div className="relative flex justify-center items-center h-75 w-60 rounded-2xl overflow-hidden ">
               <img
                 src="src/assets/moldura.png"
                 alt="Moldura decorativa"
-                className="absolute left-1/2 top-1/2 w-[130%] h-[150%] 
-                            -translate-x-1/2 -translate-y-1/2 
-                            object-contain object-center rounded-2xl z-0"
+                className="absolute left-1/2 top-1/2 w-[130%] h-[150%] -translate-x-1/2 -translate-y-1/2 object-contain object-center rounded-2xl z-0"
               />
               <img
                 src="src/assets/image-login.png"
