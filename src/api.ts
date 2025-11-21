3bc85ecb1ccb2854713d0d9d2272f308bf947147
Gabriel Evangelista <evangelistagabriel798@gmail.com>
2025-11-21 18:45:56 -0300

    refactor: correção na identação

--- src/api.ts
+++ src/api.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const api = axios.create({
-baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 
-    api.interceptors.request.use((config) => {
-    const token = Cookies.get("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-    });
+api.interceptors.request.use((config) => {
+  const token = Cookies.get("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
